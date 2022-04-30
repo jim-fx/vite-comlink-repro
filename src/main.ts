1f6ccaa4ea1b9f695ae69a35e89de5963f6c1237
Max <jim-x@web.de>
2022-04-30 20:24:49 +0200

    feat: init

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+const instance = new ComlinkWorker<typeof import('./worker')>(new URL('./worker', import.meta.url), {/* normal Worker options*/ })
+const result = await instance.add(2, 3)
+
+app.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <b>Worker: ${result}</b>
+`
